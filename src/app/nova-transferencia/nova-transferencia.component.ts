9b735f229995168b9fde64453d9d7bbecc9f6645
tarikleite <tarikleite2@gmail.com>
2022-06-29 14:06:18 -0300

    Pasta de componente HTML criada,

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,8 @@
+@Component({
+  selector: 'nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class NovaTransferenciaComponent  {
+  
+}
